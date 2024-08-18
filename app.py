17670e004b84cab2415951124b0756421167c8ac
Rudik Arakelyan <rudik.arakelyan777@gmail.com>
2024-08-18 13:44:53 -0700

    app.py changes

--- app.py
+++ app.py
@@ -19,212 +19,58 @@ load_dotenv()
 app = Flask(__name__)
 
 
-def voice_agent(fileName):
-    client = speech.SpeechClient()
-    with io.open(fileName, "rb") as f:
-        content = f.read()
-        audio = speech.RecognitionAudio(content=content)
-
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.MP3,
-        sample_rate_hertz=16000,
-        language_code="en-US",
-    )
-
-    groqClient = Groq(
-        # This is the default and can be omitted
-        api_key=os.getenv("GROQ_API_KEY"),
-    )
-
-    response = client.recognize(config=config, audio=audio)
-
-    system_prompt = """Given a user's complaint as a text transcription gotten from a video, try to extract the information from the transcript and return the
-    extracted values as a json. Only look for the keys specified. Try as much as possible to get the required information but 
-    if it is not available you can set the value to null.
-
-    Return specifically in the JSON format below:
-    {
-        "_index": "complaint-public-v2",
-        "_type": "_doc",
-        "_score": null,
-        "_source":
-        {
-            "product": str, 
-            "complaint_what_happened": (The user's complaint)
-            "date_sent_to_company": str(YYYY-MM-DDT12HH:MM-HH:MM)
-            "issue": str, 
-            "sub_product": str, 
-            "sub_issue": str,
-            "zip_code": str,
-            "tags": str,
-            "complaint_id": str,
-            "timely": (Yes/No),
-            "consumer_consent_provided": ("Consent provided"/"Consent Not Provided"),
-            "company_response": str,
-            "submitted_via": str ('web', 'letter', etc),
-            "company": str,
-            "date_received": str(YYYY-MM-DDT12HH:MM-HH:MM),
-            "state": str (NY,SA,etc),
-            "consumer_disputed": str,
-            "company_public_response": str
-        }
-    }
-    
-    """
-
-    chat_completion = groqClient.chat.completions.create(
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {
-                "role": "user",
-                "content": "Categorize this complaint: {0}".format(
-                    response
-                ),
-            },
-        ],
-        model="llama3-8b-8192",
-        response_format={"type": "json_object"},
-    )
-    
-    return chat_completion.choices[0].message.content
-
-def text_agent(complaint: str):
-    groqClient = Groq(
-        # This is the default and can be omitted
-        api_key=os.getenv("GROQ_API_KEY"),
-    )
-
-    system_prompt = """Given a user's complaint as a message they sent, try to extract the information from the transcript and return the
-    extracted values as a json. Only look for the keys specified. Try as much as possible to get the required information but 
-    if it is not available you can set the value to null.
-
-    Return specifically in the JSON format below:
-    {
-        "_index": "complaint-public-v2",
-        "_type": "_doc",
-        "_score": null,
-        "_source":
-        {
-            "product": str, 
-            "complaint_what_happened": (The user's complaint)
-            "date_sent_to_company": str(YYYY-MM-DDT12HH:MM-HH:MM)
-            "issue": str, 
-            "sub_product": str, 
-            "sub_issue": str,
-            "zip_code": str,
-            "tags": str,
-            "complaint_id": str,
-            "timely": (Yes/No),
-            "consumer_consent_provided": ("Consent provided"/"Consent Not Provided"),
-            "company_response": str,
-            "submitted_via": str ('web', 'letter', etc),
-            "company": str,
-            "date_received": str(YYYY-MM-DDT12HH:MM-HH:MM),
-            "state": str (NY,SA,etc),
-            "consumer_disputed": str,
-            "company_public_response": str
-        }
-    }
-    
-    """
-
-    chat_completion = groqClient.chat.completions.create(
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {
-                "role": "user",
-                "content": "Categorize this complaint: {0}".format(
-                    complaint
-                ),
-            },
-        ],
-        model="llama3-8b-8192",
-        response_format={"type": "json_object"},
-    )
-    
-    return chat_completion.choices[0].message.content
-
-def image_agent(path):
-    
-    client = vision.ImageAnnotatorClient()
-    with open(path, "rb") as image_file:
-        content = image_file.read()
-    
-    image = vision.Image(content=content)
-    
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    # print(' '.join([text.description for text in texts]))
-    whole_text = ' '.join([text.description for text in texts])
-
-    if response.error.message:
-        raise Exception(
-            "{}\nFor more info on error messages, check: "
-            "https://cloud.google.com/apis/design/errors".format(response.error.message)
-        )
-    
-    groqClient = Groq(
-        # This is the default and can be omitted
-        api_key=os.getenv("GROQ_API_KEY"),
-    )
-
-    system_prompt = """Given a the text gotten from the screenshot of a user's complaint, try to extract the information from the transcript and return the
-    extracted values as a json. Only look for the keys specified. Try as much as possible to get the required information but 
-    if it is not available you can set the value to null.
-
-    Return specifically in the JSON format below:
-    {
-        "_index": "complaint-public-v2",
-        "_type": "_doc",
-        "_score": null,
-        "_source":
-        {
-            "product": str, 
-            "complaint_what_happened": (The user's complaint)
-            "date_sent_to_company": str(YYYY-MM-DDT12HH:MM-HH:MM)
-            "issue": str, 
-            "sub_product": str, 
-            "sub_issue": str,
-            "zip_code": str,
-            "tags": str,
-            "complaint_id": str,
-            "timely": (Yes/No),
-            "consumer_consent_provided": ("Consent provided"/"Consent Not Provided"),
-            "company_response": str,
-            "submitted_via": str ('web', 'letter', etc),
-            "company": str,
-            "date_received": str(YYYY-MM-DDT12HH:MM-HH:MM),
-            "state": str (NY,SA,etc),
-            "consumer_disputed": str,
-            "company_public_response": str
-        }
-    }
-    
-    """
-
-    chat_completion = groqClient.chat.completions.create(
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {
-                "role": "user",
-                "content": "Categorize this complaint: {0}".format(
-                    whole_text
-                ),
-            },
-        ],
-        model="llama3-8b-8192",
-        response_format={"type": "json_object"},
-    )
-    
-    return chat_completion.choices[0].message.content
-    
-
-
-
 @app.route("/categorize", methods=["POST"])
 def categorize_complaint():
     data = request.get_json()
-    user_complaint: str = data["prompt"]
+    
+    def voice_agent(file_path):
+        client = speech.SpeechClient()
+        with io.open(file_path, "rb") as f:
+            content = f.read()
+            audio = speech.RecognitionAudio(content=content)
+
+        config = speech.RecognitionConfig(
+            encoding=speech.RecognitionConfig.AudioEncoding.MP3,
+            sample_rate_hertz=16000,
+            language_code="en-US",
+        )
+
+        response = client.recognize(config=config, audio=audio)
+        return voice_agent_process(response)
+
+    def text_agent(complaint: str):
+        return text_agent_process(complaint)
+
+    def image_agent(file_path):
+        client = vision.ImageAnnotatorClient()
+        with open(file_path, "rb") as image_file:
+            content = image_file.read()
+        
+        image = vision.Image(content=content)
+        
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+        whole_text = ' '.join([text.description for text in texts])
+
+        if response.error.message:
+            raise Exception(
+                "{}\nFor more info on error messages, check: "
+                "https://cloud.google.com/apis/design/errors".format(response.error.message)
+            )
+        
+        return image_agent_process(whole_text)
+
+    audio_result = voice_agent(data.get("audio")) if data.get("audio") else None
+    image_result = image_agent(data.get("image")) if data.get("image") else None
+    text_result = text_agent(data.get("text")) if data.get("text") else None
+
+    user_complaint = ""
+    if audio_result:
+        user_complaint += f"Audio Complaint: {audio_result}\n"
+    if image_result:
+        user_complaint += f"Image Complaint: {image_result}\n"
+    if text_result:
+        user_complaint += f"Text Complaint: {text_result}\n"
 
     esClient = Elasticsearch(
         cloud_id=os.getenv("ELASTIC_SEARCH_CLOUD_ID"),
@@ -232,7 +78,6 @@ def categorize_complaint():
     )
 
     groqClient = Groq(
-        # This is the default and can be omitted
         api_key=os.getenv("GROQ_API_KEY"),
     )
 
@@ -249,10 +94,7 @@ def categorize_complaint():
         },
     )
 
-    related_ids = []
-
-    for hit in response["hits"]["hits"]:
-        related_ids.append(hit["_id"])
+    related_ids = [hit["_id"] for hit in response["hits"]["hits"]]
 
     related_records = []
     connection = None
@@ -276,19 +118,17 @@ def categorize_complaint():
             connection.close()
             print("Database conn terminated")
 
-    related_records_str = ""
+    related_records_str = "\n".join([json.dumps(record[4]) for record in related_records])
 
-    for record in related_records:
-        related_records_str += json.dumps(record[4]) + "\n"
-
-    system_prompt = """Given a user's complaint as a regular text and a couple of stored similar complaints, categorize the 
+    system_prompt = """Given a user's complaint (which may include information from audio, image, and text sources) and a couple of stored similar complaints, categorize the 
     current user's complaint using the following keys in the similar complaints: 'product', 'issue', 'subproduct', and 'subissue'.
     Try as much as possible to use one of the given values of each of these keys only when they are related, otherwise you can form the best value for it.
+    Take into account all the information provided from different sources (audio, image, text) when categorizing.
 
     Return specifically in the JSON format below:
     {
         "product": str, 
-        "complaint_what_happened": (The user's complaint)
+        "complaint_what_happened": (The user's complaint, combining information from all sources)
         "issue": str, 
         "sub_product": str, 
         "sub_issue": str, 
@@ -313,16 +153,5 @@ def categorize_complaint():
     return chat_completion.choices[0].message.content
 
 
-@app.route("/agent", methods=["POST"])
-def agent_extract():
-
-    data = request.get_json()
-
-    return ""
-
-
 if __name__ == "__main__":
-    # app.run(debug=True)
-    # voice_agent("Text-to-Speech_18-Aug-2024_07-28.mp3")
-    # text_agent("""I took out an auto loan with ABC Bank, and my monthly payments were set to be automatically deducted from my checking account. However, last month, the payment was not deducted, and I was charged a late fee of $50. I contacted the bank immediately to resolve the issue, but they insisted that I missed the payment, despite my bank statements clearly showing sufficient funds in my account. I requested a refund of the late fee, but the bank refused, stating that it was my responsibility to ensure the payment was made on time. I escalated the matter to a supervisor, who promised to investigate and get back to me within 48 hours, but I never heard back. Frustrated, I decided to cancel the auto-payment feature altogether and manually make the payment. Unfortunately, the bank charged me another late fee the following month, claiming that I had not made the payment on time, even though I had a confirmation number from their online banking system. I am now being harassed by collections, and my credit score has been negatively impacted due to these false reports. This issue has caused me significant stress and financial burden, and I want the late fees refunded, the negative credit report corrected, and compensation for the distress caused.""")
-    image_agent("Screenshot (14).png")
+    app.run(debug=True)
